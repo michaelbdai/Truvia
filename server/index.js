563dc75de5460c9e8d0d56a51abfa3d34a2c49a9
Louis Bergeron <louispvb@gmail.com>
2016-12-14 17:08:38 -0800

    (Feature) server serves static files correctly, sockets partly working

--- server/index.js
+++ server/index.js
@@ -1,41 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-// const session = require('express-session');
 const cors = require('cors');
 require('isomorphic-fetch');
 
-const triviaRoute = require('./routes');
-
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+
+const triviaRoute = require('./routes');
 const port = process.env.PORT || 8080;
 global.jwtSecret = process.env.JWT_SECRET || 'HoorayTeamSpinach';
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('client/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }));
-
 app.use(cors());
 
 app.use('/', triviaRoute);
 
-app.listen(port);
+const server = app.listen(port);
+const io = require('socket.io')(server);
+
 console.log("Server listening on port " + port);
 
-/////////////////////////////////////////////////////////////////////////
-// Sockets
-io.on('connection', (socket) => {
-  socket.broadcast.emit('A user has connected');
-});
+module.exports = {app, io};
 
-module.exports = app;
+// Run the socket connections
+require('./controllers/socket/rooms');
