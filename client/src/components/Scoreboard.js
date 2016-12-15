93d37d97bbd1bb86e4415456c4e44e8e6c9a38e8
Hong <keepthemonochrome@gmail.com>
2016-12-14 17:37:57 -0800

    Complete rough UI version of entire Trivia page

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import Styles from './Styles'
 
 const Scoreboard = () => (
-
-	<div>Scoreboard</div>
-
-
+  <Paper style={Styles.scoreboardContainer} >
+    <Table >
+    <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+      <TableRow >
+        <TableHeaderColumn>Ranking</TableHeaderColumn>
+        <TableHeaderColumn>Name</TableHeaderColumn>
+        <TableHeaderColumn>Score</TableHeaderColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody displayRowCheckbox={false}>
+      <TableRow>
+        <TableRowColumn>1</TableRowColumn>
+        <TableRowColumn>Susan Hong</TableRowColumn>
+        <TableRowColumn>10</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>2</TableRowColumn>
+        <TableRowColumn>Bing Dai</TableRowColumn>
+        <TableRowColumn>6</TableRowColumn>
+      </TableRow>
+    </TableBody>
+    </Table>
+  </Paper>
 );
 
 export default Scoreboard
+
+ // <Paper style={Styles.scoreboardContainer} zDepth={1} >
+ //      <List>
+ //        <Subheader>Score Board</Subheader>
+ //        <ListItem
+ //          primaryText="Brendan Lim"
+ //        />
+ //      </List>
+ //    </Paper>
